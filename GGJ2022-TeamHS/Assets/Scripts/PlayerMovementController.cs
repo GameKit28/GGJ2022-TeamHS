6ff96e01b8ec3b54f98840b84ede3b788c7b71c7
Delton Hulbert <hulbert.delton@yahoo.com>
2022-01-29 14:39:44 -0700

    Updating to use the new input system
    also commented out old player control code, needs to use the new system, which is set to pub/sub

--- GGJ2022-TeamHS/Assets/Scripts/PlayerMovementController.cs
+++ GGJ2022-TeamHS/Assets/Scripts/PlayerMovementController.cs
@@ -7,25 +7,20 @@ public class PlayerMovementController : MonoBehaviour
     public float playerSpeed = 0.005f;
     public Camera trackingCamera;
 
-    private Vector3 movementPlaneScalar = new Vector3(1, 0, 1); //Up is the Y Axis. Camera moves along the X,Z plane
+    //private Vector3 movementPlaneScalar = new Vector3(1, 0, 1); //Up is the Y Axis. Camera moves along the X,Z plane
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 inputVector = new Vector2(
-                Input.GetAxis("Horizontal"),
-                Input.GetAxis("Vertical"));
+        //Vector2 inputVector = new Vector2(
+        //        Input.GetAxis("Horizontal"),
+        //        Input.GetAxis("Vertical"));
 
-        Vector3 cameraPlanalForward = Vector3.Scale(trackingCamera.transform.forward, movementPlaneScalar).normalized;
-        Vector3 cameraPlanalRight = Vector3.Scale(trackingCamera.transform.right, movementPlaneScalar).normalized;
-        Vector3 movementVector = (cameraPlanalForward * inputVector.y) + (cameraPlanalRight * inputVector.x);
+        //Vector3 cameraPlanalForward = Vector3.Scale(trackingCamera.transform.forward, movementPlaneScalar).normalized;
+        //Vector3 cameraPlanalRight = Vector3.Scale(trackingCamera.transform.right, movementPlaneScalar).normalized;
+        //Vector3 movementVector = (cameraPlanalForward * inputVector.y) + (cameraPlanalRight * inputVector.x);
 
-        transform.Translate(movementVector * playerSpeed);
+        //transform.Translate(movementVector * playerSpeed);
     }
 }
