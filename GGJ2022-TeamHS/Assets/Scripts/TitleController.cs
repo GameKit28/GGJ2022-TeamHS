a7e5889c92945932c9aa7b1592b98a01e2932c87
Connor Botts <cbotts@trickyfast.com>
2022-01-29 15:22:58 -0500

    more work on UI framework for titlescreen

--- GGJ2022-TeamHS/Assets/Scripts/TitleController.cs
+++ GGJ2022-TeamHS/Assets/Scripts/TitleController.cs
@@ -1,74 +1,93 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.UI;
 
 public class TitleController : MonoBehaviour
 {
-
+    [Header("Button Config")]
     public Button playButton;
     public Button quitButton;
     public Button optionsButton;
-
+    [Space]
+    [Header("Panel Config")]
+    public GameObject mainButtonsPanel;
     public GameObject optionsPanel;
     public GameObject levelButtonsPanel;
-    public GameObject mainButtonsPanel;
-
+    [Space]
+    [Header("Popup Config")]
     public GameObject popup;
     public TMPro.TMP_Text popupText;
-    public Button popupLeftButton;
-    public TMPro.TMP_Text popupLeftButtonText;
-    public Button popupRightButton;
-    public TMPro.TMP_Text popupRightButtonText;
+    public LayoutGroup popupButtonsPanel;
+    public Button popupButtonPrefab;
+
+    private struct PopupButton
+    {
+        public string text;
+        public UnityAction onClickAction;
+    }
 
     //TODO One assumption I'm going to make is that hitting B or ESC on the main menu will bring you back to the main buttons panel -cb
     void Start()
     {
-        try
-        {
-            playButton.onClick.AddListener(OnPlayClicked);
-            optionsButton.onClick.AddListener(OnOptionsClicked);
-            quitButton.onClick.AddListener(OnQuitClicked);
-        }
-        catch
-        {
-            Debug.LogWarning("Title Screen button refs are missing in the inspector so some of them prob wont work.");
-        }
 
+        playButton.onClick.AddListener(OnPlayClicked);
+        optionsButton.onClick.AddListener(OnOptionsClicked);
+        quitButton.onClick.AddListener(OnQuitClicked);
     }
 
     void OnPlayClicked()
     {
-        try
-        {
-            mainButtonsPanel.SetActive(false);
-            levelButtonsPanel.SetActive(true);
-        }
-        catch
-        {
-            Debug.LogWarning("Title Screen panel refs are missing in the inspector so some of them prob wont work.");
 
-        }
+        mainButtonsPanel.SetActive(false);
+        levelButtonsPanel.SetActive(true);
     }
 
     void OnOptionsClicked()
     {
-        try
-        {
-            mainButtonsPanel.SetActive(false);
-            optionsPanel.SetActive(true);
-        }
-        catch
-        {
-            Debug.LogWarning("Title Screen panel refs are missing in the inspector so some of them prob wont work.");
-        }
+
+        mainButtonsPanel.SetActive(false);
+        optionsPanel.SetActive(true);
     }
 
     void OnQuitClicked()
     {
         //TODO Save progress here or we should prob just save as we go? -cb
-        Application.Quit();
+        ShowPopup("Are you sure you want to quit?", new PopupButton[]
+        {
+            new PopupButton{text = "Cancel"},
+            new PopupButton{text = "Quit", onClickAction = Application.Quit}
+        }); ;
     }
 
+    void ShowPopup(string bodyText, params PopupButton[] buttons)
+    {
 
+        popupText.text = bodyText;
+        popupButtonsPanel.enabled = false;
+
+        foreach (var button in buttons)
+        {
+            var buttonInstance = Instantiate(popupButtonPrefab, popupButtonsPanel.transform);
+            buttonInstance.GetComponentInChildren<TMPro.TMP_Text>().text = button.text;
+            buttonInstance.onClick.AddListener(() =>
+            {
+                button.onClickAction?.Invoke();
+                HidePopup();
+            });
+        }
+
+        popupButtonsPanel.enabled = true;
+        popup.SetActive(true);
+    }
+
+    void HidePopup()
+    {
+        popup.SetActive(false);
+        foreach(Transform childButtons in popupButtonsPanel.transform)
+        {
+            Destroy(childButtons.gameObject);
+        }
+    }
 }
