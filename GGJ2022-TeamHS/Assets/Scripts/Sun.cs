ee2a6d63635f37589e7a8a74aae8fdb166b40112
Delton Hulbert <hulbert.delton@yahoo.com>
2022-01-30 08:11:57 -0700

    updating the sun with new color change

--- GGJ2022-TeamHS/Assets/Scripts/Sun.cs
+++ GGJ2022-TeamHS/Assets/Scripts/Sun.cs
@@ -6,10 +6,15 @@ using UnityEngine;
 public class Sun : MonoBehaviour
 {
     public float rotationRate = 15f;
+    private Light light;
+    private Coroutine SunLerper;
+    private float originalLightTemp;
 
     // Start is called before the first frame update
     void Start()
     {
+        light = GetComponent<Light>();
+        originalLightTemp = light.colorTemperature;
         SunManager.RegisterSun(this);
     }
 
@@ -22,5 +27,29 @@ public class Sun : MonoBehaviour
     public Vector3 GetShadowVector()
     {
         return transform.forward;
+    }
+    public void ChangeSunTemp(float temp) {
+        SunLerper = StartCoroutine(LerpSunColor(temp));
+    }
+
+    public void ResetSunTemp() {
+        SunLerper = StartCoroutine(LerpSunColor(originalLightTemp));
+
+    }
+
+    private void EndIt() {
+        StopCoroutine(SunLerper);
+    }
+    private IEnumerator LerpSunColor(float newTemp) {
+        float currTemp = light.colorTemperature;
+        float t = 0f;
+
+        while (t <= 1f) {
+            t += Time.deltaTime / 4;
+            light.colorTemperature = Mathf.Lerp(currTemp, newTemp, t);
+            yield return null;
+        }
+
+        EndIt();
     }
 }
