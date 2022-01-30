65b9f69d39d06c2aad65f1cd3f0903ecc00329a5
Connor Botts <cbotts@trickyfast.com>
2022-01-29 22:09:02 -0500

    cinemachine integration

--- GGJ2022-TeamHS/Assets/Scripts/Player/PlayerMovementController.cs
+++ GGJ2022-TeamHS/Assets/Scripts/Player/PlayerMovementController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerMovementController : MonoBehaviour
 {
     public float playerSpeed = 0.005f;
@@ -10,11 +11,12 @@ public class PlayerMovementController : MonoBehaviour
 
     private Vector3 movementPlaneScalar = new Vector3(1, 0, 1); //Up is the Y Axis. Camera moves along the X,Z plane
     private Vector2 inputVector = Vector2.zero;
+    private Rigidbody body;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        body = GetComponent<Rigidbody>();
     }
 
     public void OnMove(InputValue input)
@@ -29,6 +31,8 @@ public class PlayerMovementController : MonoBehaviour
         Vector3 cameraPlanalRight = Vector3.Scale(trackingCamera.transform.right, movementPlaneScalar).normalized;
         Vector3 movementVector = (cameraPlanalForward * inputVector.y) + (cameraPlanalRight * inputVector.x);
 
-        transform.Translate(movementVector * playerSpeed);
+        body.velocity = (movementVector * playerSpeed);
+        var newRotation = Quaternion.Euler(0, trackingCamera.transform.eulerAngles.y, 0);
+        body.transform.rotation = Quaternion.Lerp(body.transform.rotation, newRotation, Time.deltaTime * 3);
     }
 }
