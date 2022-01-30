2bcaaa87ac465b343cde3da7e2024decd4cbfbee
Connor Botts <cbotts@trickyfast.com>
2022-01-30 14:04:58 -0500

    sounds and level work

--- GGJ2022-TeamHS/Assets/Scripts/LevelController.cs
+++ GGJ2022-TeamHS/Assets/Scripts/LevelController.cs
@@ -15,18 +15,26 @@ public class LevelController : MonoBehaviour
     public CinemachineVirtualCamera followCam;
     public CinemachineVirtualCamera mapCam;
     public TMPro.TMP_Text startText;
+    public int nextLevelIndex;
 
     private bool levelStarted = false;
-    private bool disableCamToggling = false;
+    private bool endOfLevel = false;
+    private InputAction toggleAction;
 
     public void Start()
     {
-        player.GetComponent<PlayerInput>().actions["ToggleCamera"].performed += ToggleCamera;
+        toggleAction = player.GetComponent<PlayerInput>().actions["ToggleCamera"];
+        toggleAction.performed += ToggleCamera;
+        var displayString = toggleAction.GetBindingDisplayString();
+        startText.text = $"Press {displayString} to Start!";
     }
 
     private void ToggleCamera(InputAction.CallbackContext obj)
     {
-        if (disableCamToggling) return;
+        if (endOfLevel) 
+        {
+            SceneManager.LoadScene(nextLevelIndex);
+        };
 
         if (!levelStarted) 
         {
@@ -47,13 +55,13 @@ public class LevelController : MonoBehaviour
         if(other.tag == "Player")
         {
             //TODO win logic here
-            disableCamToggling = true;
+            endOfLevel = true;
             mapCam.enabled = true;
-            startText.text = "Ya win bitch!";
+            startText.text = $"Ya win bitch!";
             DOTween.Sequence()
                 .Append(startText.DOColor(Color.white, 1.5f))
                 .Append(startText.rectTransform.DOPunchScale(Vector3.one * 2, 3))
-                .OnComplete(() => SceneManager.LoadScene(0));
+                .OnComplete(() => startText.text = $"Press {toggleAction.GetBindingDisplayString()} to Continue!");
         }
     }
 }
