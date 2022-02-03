9cf1b3f780246f7f83161073e42eafa086a5bbff
Delton Hulbert <hulbert.delton@yahoo.com>
2022-02-02 22:03:38 -0700

    Smoothing input vector for animation
    smoothing changes to animator on keyboard input. Because it's lerping through the "dead zone", purely horizontal movement currently resets the animator to Idle temporarily. Just need to change the "is moving" boolean set to check if the player is giving movement imput.

--- GGJ2022-TeamHS/Assets/Scripts/CatAnimator.cs
+++ GGJ2022-TeamHS/Assets/Scripts/CatAnimator.cs
@@ -7,6 +7,7 @@ using UnityEngine;
 public class CatAnimator : MonoBehaviour
 {
     private AudioSource audioSource;
+    private Vector2 velocity;
 
     const float motion_threshold_speed = 0.1f;
     // private reference to player movement type
@@ -28,15 +29,16 @@ public class CatAnimator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector2 velocity = movement_controller.inputVector;
-        // TODO get forward velocity instead of input velocity
+        Vector2 veloIn = movement_controller.inputVector;
+        // TODO get forward velocity instead of input velowwwwwwwwcity
         
-        bool is_moving = velocity.magnitude > motion_threshold_speed;
+        bool is_moving = veloIn.magnitude > motion_threshold_speed;
         if (is_moving)
             anim.SetBool("Move", true);
         else    
-            anim.SetBool("Move", false);        
+            anim.SetBool("Move", false);
 
+        velocity = new Vector2(Mathf.Lerp(velocity.x,veloIn.x,0.1f),Mathf.Lerp(velocity.y,veloIn.y,0.2f));
         anim.SetFloat("VelX", velocity.x);
         anim.SetFloat("VelY", velocity.y);        
 
